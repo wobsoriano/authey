d3086e691e45b76dcc3a0787aa64d75753732074
wobsoriano <sorianorobertc@gmail.com>
2022-12-16 10:45:44 -0800

    add initial tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,51 @@
-import { describe, expect, it } from 'vitest'
+import type { SuperTest, Test } from 'supertest'
+import supertest from 'supertest'
+import { beforeEach, describe, expect, test } from 'vitest'
+import type { App } from 'h3'
+import { createApp, fromNodeMiddleware, toNodeListener } from 'h3'
+import GithubProvider from '@auth/core/providers/github'
+import type { AuthOptions } from '@auth/core'
+import { createAuthMiddleware } from '../src'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+const authOptions: AuthOptions = {
+  secret: 'SOME_SECRET_FOR_TESTING',
+  trustHost: true,
+  providers: [
+    GithubProvider({
+      clientId: '',
+      clientSecret: '',
+    }),
+  ],
+}
+
+// IN PROGRESS
+
+describe('REST APIs', () => {
+  let app: App
+  let request: SuperTest<Test>
+
+  beforeEach(() => {
+    app = createApp({ debug: false })
+    app.use(fromNodeMiddleware(createAuthMiddleware(authOptions)))
+    request = supertest(toNodeListener(app))
+  })
+
+  describe('/api/auth/* returns 200 OK', () => {
+    test('/session', async () => {
+      const res = await request.get('/api/auth/session')
+      expect(res.status).toEqual(200)
+    })
+    test('/signin', async () => {
+      const res = await request.get('/api/auth/signin')
+      expect(res.status).toEqual(200)
+    })
+    test('/signout', async () => {
+      const res = await request.get('/api/auth/signout')
+      expect(res.status).toEqual(200)
+    })
+    test('/providers', async () => {
+      const res = await request.get('/api/auth/providers')
+      expect(res.status).toEqual(200)
+    })
   })
 })
