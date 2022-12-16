4ed7e3f074ff0c193a0f8b5278bdefc6486b7a83
wobsoriano <sorianorobertc@gmail.com>
2022-12-16 13:01:51 -0800

    more simplifications

--- src/request.ts
+++ src/request.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from 'node:http'
 import getURL from 'requrl'
+import send from '@polka/send-type'
 
 export function createNodeHeaders(requestHeaders: IncomingMessage['headers']): Headers {
   const headers = new Headers()
@@ -34,14 +35,10 @@ export function createNodeRequest(req: IncomingMessage): Request {
   return new Request(url.href, init)
 }
 
-// Taken from https://github.com/hattipjs/hattip/blob/main/packages/adapter/adapter-node/src/common.ts
 export async function sendNodeResponse(
   res: ServerResponse,
   nodeResponse: Response,
 ): Promise<void> {
-  res.statusMessage = nodeResponse.statusText
-  res.statusCode = nodeResponse.status
-
   // @ts-expect-error: Node-fetch non-spec method returning all headers and their values as array
   for (const [key, values] of Object.entries(nodeResponse.headers.raw())) {
     // @ts-expect-error: Node-fetch non-spec method returning all headers and their values as array
@@ -49,46 +46,5 @@ export async function sendNodeResponse(
       res.setHeader(key, value)
   }
 
-  const contentLengthSet = nodeResponse.headers.get('content-length')
-  if (nodeResponse.body) {
-    if (contentLengthSet) {
-      for await (let chunk of nodeResponse.body as any) {
-        chunk = Buffer.from(chunk)
-        res.write(chunk)
-      }
-    }
-    else {
-      const reader = (
-        nodeResponse.body as any as AsyncIterable<Buffer | string>
-      )[Symbol.asyncIterator]()
-
-      const first = await reader.next()
-      if (first.done) {
-        res.setHeader('content-length', '0')
-      }
-      else {
-        const secondPromise = reader.next()
-        let second = await Promise.race([
-          secondPromise,
-          Promise.resolve(null),
-        ])
-
-        if (second && second.done) {
-          res.setHeader('content-length', first.value.length)
-          res.write(first.value)
-        }
-        else {
-          res.write(first.value)
-          second = await secondPromise
-          for (; !second.done; second = await reader.next())
-            res.write(Buffer.from(second.value))
-        }
-      }
-    }
-  }
-  else if (!contentLengthSet) {
-    res.setHeader('content-length', '0')
-  }
-
-  res.end()
+  return send(res, nodeResponse.status, await nodeResponse.text())
 }
