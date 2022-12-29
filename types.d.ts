c023d0a523325a23f1b9946ef04743c6c0f186da
wobsoriano <sorianorobertc@gmail.com>
2022-12-29 10:48:15 -0800

    update @polka/send-type types

--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,5 @@
-declare module '@polka/send-type'
+declare module '@polka/send-type' {
+  import { ServerResponse } from 'node:http'
+  function send(res: ServerResponse, status: number, data: string, headers?: Record<string, string>): void
+  export = send
+}
