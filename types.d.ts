30cd7030e75fb0538037cd9952c68d1b6782a02c
wobsoriano <sorianorobertc@gmail.com>
2023-10-20 09:48:21 -0700

    fix types

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,6 @@
 declare module '@polka/send-type' {
-  import { ServerResponse } from 'node:http'
+  import type { ServerResponse } from 'node:http'
+
   function send(res: ServerResponse, status: number, data: string, headers?: Record<string, string>): void
-  export = send
+  export default send
 }
